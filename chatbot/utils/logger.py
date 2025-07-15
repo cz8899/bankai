d5d33f8082e30e3e68987f68416c15b59e3ac7ff
cz8899 <chriszheng9@gmail.com>
2025-07-15 17:02:56 -0400

    Update and rename logger.py to logger_old.py
    no longer need logger.py under chatbot/utils/

--- chatbot/utils/logger.py
+++
@@ -1,9 +0,0 @@
-# chatbot/utils/logger.py
-import logging
-
-logger = logging.getLogger("devgenius")
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s — %(levelname)s — %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
